c4492b688ff1165b8c0576665e154315823eaa4d
Roy van Kaathoven <royvankaathoven@hotmail.com>
2013-09-03 20:52:02 +0200

    spacing

--- test/rest.php
+++ test/rest.php
@@ -6,23 +6,23 @@ $fileLocation = realpath("files/$filename");
 $fileinfo = pathinfo($fileLocation);
 
 if (!is_dir($fileinfo['dirname'])) {
-	mkdir($fileinfo['dirname']);
+    mkdir($fileinfo['dirname']);
 }
 
 switch($_SERVER['REQUEST_METHOD']) {
 
-	case "GET":
-		header($_SERVER["SERVER_PROTOCOL"] . " 200 OK");
+    case "GET":
+        header($_SERVER["SERVER_PROTOCOL"] . " 200 OK");
         header("Cache-Control: public");
         //header("Content-Type: application/zip");
         header("Content-Transfer-Encoding: Binary");
         header("Content-Length:".filesize($fileLocation));
         header("Content-Disposition: attachment; filename=" . $fileinfo['filename']);
         readfile($fileLocation);
-		break;
+        break;
 
-	case "POST":
-		file_put_contents($fileLocation, file_get_contents("php://input"));
-		echo 'done!';
-		break;
+    case "POST":
+        file_put_contents($fileLocation, file_get_contents("php://input"));
+        echo 'done!';
+        break;
 }
