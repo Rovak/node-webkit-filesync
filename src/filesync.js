2bc3bb2ad8fed19c746eb25c97d462361b888fb2
Roy van Kaathoven <royvankaathoven@hotmail.com>
2013-09-03 19:35:02 +0200

    backup

--- src/filesync.js
+++ src/filesync.js
@@ -5,71 +5,126 @@ var fs = require('fs'),
     util = require('util'),
     http = require('http'),
     events = require('events'),
-    child_process = require('child_process'),
-    sync = function() {};
+    child_process = require('child_process');
 
-sync.prototype = new events.EventEmitter();
 
-sync.prototype.folder = process.cwd();
+function watchedFile(filename, id) {
+  this.filename = filename;
+  this.id = id;
+}
 
-sync.prototype.watchFile = function(filename) {
-  var me = this;
-  fs.watch(filename, function(event, name) {
-    if (filename) {
-      sys.puts("changed found: " + filename + ", sending data");
-      me.sendFile(filename, "http://localhost/node-webkit-filerunner/test/post.php");
-    }
-  });
-};
+function sync(options) {
+  this.options = options || {};
+  this.folder = options.folder || process.cwd();
+}
 
-sync.prototype.sendFile = function(filename, remoteUrl) {
-
-  function send_file(data, remoteUrl) {
-    var requestUrl = url.parse(remoteUrl);
-    var post_options = {
+function send_file(file, data, remoteUrl) {
+  var me = this,
+      requestUrl = url.parse(remoteUrl),
+      post_options = {
         host: requestUrl.hostname,
         port: '80',
         path: requestUrl.path,
         method: 'POST',
         headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'Content-Length': data.length
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Content-Length': data.length
         }
-    };
+      };
 
-    sys.puts("sending data " + JSON.stringify(post_options));
-
-    // Set up the request
-    var post_req = http.request(post_options, function(res) {
-        res.setEncoding('utf8');
+  // Set up the request
+  var post_req = http.request(post_options, function (res) {
+    res.setEncoding('utf8');
+    res.on('end', function() {
+      me.emit('synced', {
+        filename: file.filename,
+        id: file.id
+      });
     });
+  });
 
-    // post the data
-    post_req.write(data);
-    post_req.end();
+  // post the data
+  post_req.write(data);
+  post_req.end();
+}
+
+sync.prototype = new events.EventEmitter();
+
+sync.prototype.watchedFiles = [];
+
+sync.prototype.id = 0;
+
+/**
+ * Stop watching the given filename
+ * @param  {[type]} filename [description]
+ * @return {[type]}          [description]
+ */
+sync.prototype.stopWatching = function (filename) {
+  fs.unwatchFile(filename);
+
+  // Remove the file from the watchlist
+  var foundFile = this.watchedFiles.indexOf(filename);
+  if (foundFile !== -1) {
+    this.watchedFiles.splice(foundFile, 1);
   }
+};
 
-  sys.puts("reading " + filename);
-  fs.readFile(filename, 'utf-8', function (err, data) {
+/**
+ * Start watching the file for changes
+ *
+ * @param  {String} filename Full pathname to the file which must be watched
+ */
+sync.prototype.watchFile = function (file) {
+  var me = this;
+  fs.watch(file.filename, function (event, name) {
+    me.sendFile(file, "http://localhost/node-webkit-filerunner/test/post.php");
+    me.emit('send', {
+      filename: file.filename,
+      id: file.id,
+      file: file
+    });
+  });
+  this.watchedFiles.push(file.filename);
+};
+
+/**
+ * Send file back to the server with a POST request
+ *
+ * @param  {[type]} filename  [description]
+ * @param  {[type]} remoteUrl [description]
+ * @return {[type]}           [description]
+ */
+sync.prototype.sendFile = function (file, remoteUrl) {
+
+  var me = this;
+
+  fs.readFile(file.filename, 'utf-8', function (err, data) {
 
     if (err) {
+      me.emit('error', err);
       console.log("FATAL An error occurred trying to read in the file: " + err);
       return;
     }
-    if(data) {
-      sys.puts("asfas");
-      send_file(data, remoteUrl);
+
+    if (data) {
+      send_file(file, data, remoteUrl);
     }
   });
 };
 
-sync.prototype.downloadAndSyncFile = function(downloadUrl) {
+/**
+ * Download and add the file to the watchlist
+ *
+ * @param  {String} downloadUrl Remote URL where the file is located
+ * @return {Void}
+ */
+sync.prototype.downloadAndSyncFile = function (downloadUrl) {
 
   var me = this,
-    requestUrl = url.parse(downloadUrl),
-    hostname = url.hostname,
-    request,
-    totalLength = 0;
+      requestUrl = url.parse(downloadUrl),
+      hostname = url.hostname,
+      request,
+      totalLength = 0;
 
   var options = {
     hostname: requestUrl.hostname,
@@ -78,31 +133,50 @@ sync.prototype.downloadAndSyncFile = function(downloadUrl) {
     method: 'GET'
   };
 
-  request = http.request(options, function(res) {
+  request = http.request(options, function (res) {
     var download_filename = options.path.split("/").pop(),
       total_length = parseInt(res.headers['content-length'], 0),
-      filename = '/var/www/node-webkit-filerunner/' + download_filename,
-      download_stream = fs.createWriteStream(filename, { flags: 'w+' });
+      fileId = me.id++,
+      filename = me.folder + download_filename,
+      download_stream = fs.createWriteStream(filename, {
+        flags: 'w+'
+      });
 
-    me.emit('start', downloadUrl);
+    me.emit('start', {
+      filename: download_filename,
+      id: fileId
+    });
 
     res.setEncoding(null); // binary
-    res.on('data', function(chunk) {
+    res.on('data', function (chunk) {
       totalLength += chunk.length;
       download_stream.write(chunk, encoding = 'binary');
-      me.emit('progress', Math.round((totalLength / total_length) * 100));
+      me.emit('progress', {
+        percentage: Math.round((totalLength / total_length) * 100),
+        filename: download_filename,
+        id: fileId
+      });
     });
-    res.on("end", function() {
+    res.on('end', function () {
       download_stream.end();
       me.emit('finished', {
-        localFile: filename
+        filename: download_filename,
+        id: fileId
       });
       child_process.spawn('xdg-open', [filename]);
-      me.watchFile(filename);
+
+      // Small delay so a change event will not be fire immediatly
+      setTimeout(function(){
+        me.watchFile(new watchedFile(filename, fileId));
+      }, 1500);
     });
   });
 
   request.end();
 };
 
-exports = module.exports = new sync();
+exports = module.exports = {
+  create: function (options) {
+    return new sync(options);
+  }
+};
